999b7ae732133462fbc02eefb57a6d5a3cfe0393
Marcus <marcus.ra.arocha@gmail.com>
2025-09-28 15:23:03 -0600

    app works now

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,8 +22,10 @@ df_filtered['Name of Incident'] = df_filtered['Name of Incident'].str.replace('U
 for events in df_filtered:
 
     random_row1 = df_filtered.sample(n=1)
+    event_1 = {random_row1["Name of Incident"].iloc[0]}
     date_1 = int(random_row1["Year"].iloc[0])
     random_row2 = df_filtered.sample(n=1)
+    event_2 = {random_row2["Name of Incident"].iloc[0]}
     date_2 = int(random_row2["Year"].iloc[0])
 
     if abs(date_2 - date_1) <= 21:
@@ -40,33 +42,33 @@ with st.container():
     def check_one():
         if date_1 < date_2:
             col1.write("Correct")
-            col1.write(date_1)
-            col1.write(date_2)
+            col1.write(f"The {event_1} took place in {date_1}")
+            col1.write(f"The {event_2} took place in {date_2}")
             st.session_state.streak +=1
 
         else:
             col1.write("Incorrect")
-            col1.write(date_1)
-            col1.write(date_2)
+            col1.write(f"The {event_2} took place in {date_2}")
+            col1.write(f"The {event_1} took place in {date_1}")
             st.session_state.streak = 0
 
     def check_two():
         if date_2 < date_1:
             col1.write("Correct")
-            col1.write(date_1)
-            col1.write(date_2)
+            col1.write(f"The {event_2} took place in {date_2}")
+            col1.write(f"The {event_1} took place in {date_1}")
             st.session_state.streak += 1
 
         else:
             col1.write("Incorrect")
-            col1.write(date_1)
-            col1.write(date_2)
+            col1.write(f"The {event_1} took place in {date_1}")
+            col1.write(f"The {event_2} took place in {date_2}")
             st.session_state.streak = 0
 
 
     col1, col2 = st.columns([1, 1])
-    col1.button(f"{random_row1["Name of Incident"].iloc[0]}",on_click=check_one, width=700)
-    col2.button(f"{random_row2["Name of Incident"].iloc[0]}\n", on_click=check_two, width=700)
+    col1.button(f"{event_1}",on_click=check_one, width=700)
+    col2.button(f"{event_2}\n", on_click=check_two, width=700)
 
 
 
